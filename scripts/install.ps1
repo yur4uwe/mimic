2ed3ae7fe7b246c4e068fb24d5452090335c6858
yur4uwe <tsg9515@example.com>
2025-12-02 01:14:29 +0200

    getting ready for release

---
+++ scripts/install.ps1
@@ -0,0 +1,1 @@
+Write-Host "Installation script is not yet implemented."
---
+++ scripts/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Future installation script for mimic on Unix-like systems
+# Currently not implemented
+echo "Installation script is not yet implemented."
